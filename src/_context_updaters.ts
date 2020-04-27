5de64c71bc12c3d8a5c88c7ce2f218b47db168ed
Joe Gasewicz <joegasewicz@gmail.com>
2020-04-27 14:59:37 +0100

    init create metadata #1

---
+++ src/_context_updaters.ts
@@ -0,0 +1,23 @@
+// Context Updaters
+// Methods to update & track the state of all inputs, validators & general form metadata
+//
+// For example:
+// const context = {
+//     // other props...
+//     metadata: {
+//          inputs: {
+//              email: {
+//                  isValid: false,
+//                  messages: ['Cannot be empty'],
+//                  value: '',
+//                  isTouched: false,
+//              },
+//              username: {
+//                  isValid: true,
+//                  messages: [],
+//                  value: 'joebloggs',
+//                  isTouched: true,
+//              }
+//          }
+//     }
+// };
