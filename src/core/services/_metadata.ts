eee19679f2cc9cf58541649a97b8212c59c9b42e
Joe Gasewicz <joegasewicz@gmail.com>
2020-05-13 20:04:11 +0100

    types extended correctly for Field class

--- src/core/services/_metadata.ts
+++ src/core/services/_metadata.ts
@@ -7,11 +7,7 @@
 //  IMPORTANT: Objects that implement AbstractMetadata DO NOT care about the IFormContext.
 //  IMPORTANT: Metadata objects only care about their own IMetadata state type (see METADATA_NAMES).
 //  IMPORTANT: Children objects of MetadataGroup care about their parent & their own state type only.
-import {
-    IFieldValidation,
-    IInputFieldMetadata,
-    METADATA_NAMES, TypeFieldValueTypes, TypeFormMetadata, TypeIFieldMetadata, TypeInputMetadata,
-} from "../../form";
+import {IFieldValidation, METADATA_NAMES} from "../../form";
 import {IValidation} from "../../validators";
 import {getFieldValueType} from "../_helpers";
 import {FIELD_NAMES} from "../../elements";
