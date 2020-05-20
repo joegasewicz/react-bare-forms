5acdce71fc0403dd88b138d042d1b1786ce74af4
Joe Gasewicz <joegasewicz@gmail.com>
2020-05-20 17:40:35 +0100

    Radio button metadata

--- showcase/main.tsx
+++ showcase/main.tsx
@@ -49,8 +49,8 @@ export class Main extends React.Component<IProps, IState> {
         email: "",
         about: "",
         terms: false,
-        radio1: true,
-        radio2: false,
+        radio1: false,
+        radio2: true,
         radio3: false,
         fruitChoice: "",
     };
@@ -71,75 +71,75 @@ export class Main extends React.Component<IProps, IState> {
                                 autoComplete="off"
                                 callback={() => console.log("Form submitted ----> ", getFileFromRef(this.myFileRef))}>
 
-                            {/*    <TextInputField*/}
-                            {/*        value={this.state.age}*/}
-                            {/*        name="age"*/}
-                            {/*        hint="Enter your age"*/}
-                            {/*        labelText="Age"*/}
-                            {/*        validators={[isFieldEmpty(2)]}*/}
-                            {/*    />*/}
+                                <TextInputField
+                                    value={this.state.age}
+                                    name="age"
+                                    hint="Enter your age"
+                                    labelText="Age"
+                                    validators={[isFieldEmpty(2)]}
+                                />
 
-                            {/*    <TextInputField*/}
-                            {/*        value={this.state.username}*/}
-                            {/*        name="username"*/}
-                            {/*        hint="Enter your username"*/}
-                            {/*        labelText="Username"*/}
-                            {/*        validators={[isFieldEmpty(5)]}*/}
-                            {/*    />*/}
+                                <TextInputField
+                                    value={this.state.username}
+                                    name="username"
+                                    hint="Enter your username"
+                                    labelText="Username"
+                                    validators={[isFieldEmpty(5)]}
+                                />
 
-                            {/*    <PasswordField*/}
-                            {/*        name="password"*/}
-                            {/*        value={this.state.password}*/}
-                            {/*        labelText="Pasword"*/}
-                            {/*        validators={[isFieldEmpty(5)]}*/}
-                            {/*    />*/}
+                                <PasswordField
+                                    name="password"
+                                    value={this.state.password}
+                                    labelText="Pasword"
+                                    validators={[isFieldEmpty(5)]}
+                                />
 
-                            {/*    <PasswordField*/}
-                            {/*    name="confirmPassword"*/}
-                            {/*    value={this.state.confirmPassword}*/}
-                            {/*    hint="Password must match"*/}
-                            {/*    labelText="Confirm Password"*/}
-                            {/*    validators={[isFieldEmpty(5), areFieldsEqual("password")]}*/}
-                            {/*/>*/}
+                                <PasswordField
+                                name="confirmPassword"
+                                value={this.state.confirmPassword}
+                                hint="Password must match"
+                                labelText="Confirm Password"
+                                validators={[isFieldEmpty(5), areFieldsEqual("password")]}
+                            />
 
-                            {/*    <EmailField*/}
-                            {/*        name="email"*/}
-                            {/*        value={this.state.email}*/}
-                            {/*        hint="Your email"*/}
-                            {/*        labelText="Please enter your email"*/}
-                            {/*        validators={[isEmailValid()]}*/}
-                            {/*    />*/}
+                                <EmailField
+                                    name="email"
+                                    value={this.state.email}
+                                    hint="Your email"
+                                    labelText="Please enter your email"
+                                    validators={[isEmailValid()]}
+                                />
 
-                            {/*    <TextAreaField*/}
-                            {/*        name="about"*/}
-                            {/*        value={this.state.about}*/}
-                            {/*        hint="Your email"*/}
-                            {/*        labelText="Must be at least 20 characters"*/}
-                            {/*        validators={[isFieldEmpty(20)]}*/}
-                            {/*    />*/}
+                                <TextAreaField
+                                    name="about"
+                                    value={this.state.about}
+                                    hint="Your email"
+                                    labelText="Must be at least 20 characters"
+                                    validators={[isFieldEmpty(20)]}
+                                />
 
-                            {/*    <CheckBoxField*/}
-                            {/*        name="terms"*/}
-                            {/*        checked={this.state.terms}*/}
-                            {/*        hint="Click to agree"*/}
-                            {/*        labelText="Agree to terms & conditions"*/}
-                            {/*        validators={[isChecked()]}*/}
-                            {/*    />*/}
+                                <CheckBoxField
+                                    name="terms"
+                                    checked={this.state.terms}
+                                    hint="Click to agree"
+                                    labelText="Agree to terms & conditions"
+                                    validators={[isChecked()]}
+                                />
 
-                            {/*    <SelectField*/}
-                            {/*        size="lg"*/}
-                            {/*        value={this.state.fruitChoice}*/}
-                            {/*        name="fruitChoice"*/}
-                            {/*        options={["banana", "apple", "orange"]}*/}
-                            {/*    />*/}
+                                <SelectField
+                                    size="lg"
+                                    value={this.state.fruitChoice}
+                                    name="fruitChoice"
+                                    options={["banana", "apple", "orange"]}
+                                />
 
-                            {/*    <FileField*/}
-                            {/*        ref={this.myFileRef}*/}
-                            {/*        hint="Must be a file"*/}
-                            {/*        labelText="Upload your file"*/}
-                            {/*        name="myFileTest"*/}
-                            {/*        validators={[isFile()]}*/}
-                            {/*    />*/}
+                                <FileField
+                                    ref={this.myFileRef}
+                                    hint="Must be a file"
+                                    labelText="Upload your file"
+                                    name="myFileTest"
+                                    validators={[isFile()]}
+                                />
 
                                 <RadioGroup name="group1">
                                     <RadioField
